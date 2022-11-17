5675c64515dc6d9e74cc0f165e476fc242451ee4
juan9900 <juanluislauretta@gmail.com>
2022-11-17 15:29:59 -0400

    Agregado fondo para el boton hamburger de menu y ajustado ancho del menu

--- js/hamburguerMenu.js
+++ js/hamburguerMenu.js
@@ -1,14 +1,15 @@
 window.onload = function (){
-    const menu_btn = document.querySelector('.hamburguer');
+    const menu_btn = document.querySelector('.hamburger-container');
+    const hamburger_icon_container = document.querySelector('.hamburguer');
     const mobile_menu = document.querySelector('.mobile-nav');
     const menu_items = document.querySelectorAll('.mobile-nav a');
     menu_btn.addEventListener('click', function (){
-        menu_btn.classList.toggle('is-active');
+        hamburger_icon_container.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active')
     });
     for (let i = 0; i < menu_items.length; i++) {
         menu_items[i].addEventListener('click',function(){
-            menu_btn.classList.toggle('is-active');
+            hamburger_icon_container.classList.toggle('is-active');
             mobile_menu.classList.toggle('is-active')
         })
             
