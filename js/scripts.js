bdcf61ef9f0fefd623aa000ff855fcdf989c3632
juan9900 <juanluislauretta@gmail.com>
2023-07-13 08:49:54 -0400

    added promo

--- js/scripts.js
+++ js/scripts.js
@@ -1,133 +1,128 @@
-
 // boton de opciones de platos
-const btnPlatesOptions = $('.promo-plates-container');
-const platesOptions = $('.promo-plates');
-const listArrow = $('.arrow-down');
+const btnPlatesOptions = $(".promo-plates-container");
+const platesOptions = $(".promo-plates");
+const listArrow = $(".arrow-down");
 
-$(btnPlatesOptions).on('click', () => {
-    $(platesOptions).toggleClass('active');
-    $(btnPlatesOptions).toggleClass('active');
-    $(listArrow).toggleClass('rotate');
-})
+$(btnPlatesOptions).on("click", () => {
+  $(platesOptions).toggleClass("active");
+  $(btnPlatesOptions).toggleClass("active");
+  $(listArrow).toggleClass("rotate");
+});
 
-const alertPlaceholder = $('#liveAlertPlaceholder')
+const alertPlaceholder = $("#liveAlertPlaceholder");
 var timerCloseAlert;
 
 // Enviar suscripcion al club forno
-$('#club-form').on('submit',(e) => {
-    firstName = $('#firstName').val();
-    lastName = $('#lastName').val();
-    email = $('#email').val();
-    phoneNumber = $('#phoneNumber').val();
-    birthdate = new Date($('#birthdate').val());
-    console.log(birthdate);
-    e.preventDefault();
+$("#club-form").on("submit", (e) => {
+  firstName = $("#firstName").val();
+  lastName = $("#lastName").val();
+  email = $("#email").val();
+  phoneNumber = $("#phoneNumber").val();
+  birthdate = new Date($("#birthdate").val());
+  console.log(birthdate);
+  e.preventDefault();
 
-    $.ajax({
-        type: "POST",
-        url: '/modules/addClient.php',
-        data: {
-            firstName,
-            lastName,
-            email,
-            phoneNumber,
-            birthdate,
-        },
-        dataType: 'json',
-        success: (data) => {
-            console.log(data);
-            if(data.status === 'success'){
-                $('.club-form-container').addClass('d-none');
-                $('.subscribed-text').removeClass('d-none');
-            }else{
-                alert(data.errors,'danger');
-                timer = setTimeout(() => {
-                    $('#liveAlertPlaceholder').fadeOut(500, function(){
-                        $(this).empty().show();
-                    })
-                },4000)
-            }
-        },
-        error: (error) => {
-            console.log('error: ' , error);
-        },
-      });
-})
+  $.ajax({
+    type: "POST",
+    url: "/modules/addClient.php",
+    data: {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      birthdate,
+    },
+    dataType: "json",
+    success: (data) => {
+      console.log(data);
+      if (data.status === "success") {
+        $(".club-form-container").addClass("d-none");
+        $(".subscribed-text").removeClass("d-none");
+      } else {
+        alert(data.errors, "danger");
+        timer = setTimeout(() => {
+          $("#liveAlertPlaceholder").fadeOut(500, function () {
+            $(this).empty().show();
+          });
+        }, 4000);
+      }
+    },
+    error: (error) => {
+      console.log("error: ", error);
+    },
+  });
+});
 
 const alert = (message, type) => {
-    let icon;
-    switch(type){
-        case 'danger':
-            icon = `<i class="fa-solid fa-xmark pe-2"></i>`
-        break;
-        case 'success':
-            icon = `<i class="fa-solid fa-check pe-2"></i>`
-        break;
+  let icon;
+  switch (type) {
+    case "danger":
+      icon = `<i class="fa-solid fa-xmark pe-2"></i>`;
+      break;
+    case "success":
+      icon = `<i class="fa-solid fa-check pe-2"></i>`;
+      break;
+  }
 
-    }   
-    
-    const wrapper = document.createElement('div')
-    wrapper.innerHTML = [
-        `<div class="alert alert-${type} alert-dismissible mb-0" role="alert" id="alert">`,
-        `   <div>${message.join("<p></p>")}</div>`,
-        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-        '</div>'
-    ].join('')
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible mb-0" role="alert" id="alert">`,
+    `   <div>${message.join("<p></p>")}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
 
-    // const wrapper = 
-    // `<div class="alert alert-${type} alert-dismissible mb-0" role="alert" id="alert">
-    //     <div>${icon}
-    //         <div>
-    //             ${message.forEach(msg => {
-    //                 return message
-    //             })}
-    //         </div>
-    //     </div>
-    //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    // </div>`
-    
-    
-    
-    if($('#liveAlertPlaceholder').is(':empty')){
-        alertPlaceholder.append(wrapper)
-    }else{
-        //If the event have been saved in less than 3 seconds, then the previous setTimeout will be deleted
-        //so the new alert has its own 3 seconds.
-        clearTimeout(timer);
-        $('#liveAlertPlaceholder').empty();
-        console.log('Emptied');
-        alertPlaceholder.append(wrapper);
-       
-    }
+  // const wrapper =
+  // `<div class="alert alert-${type} alert-dismissible mb-0" role="alert" id="alert">
+  //     <div>${icon}
+  //         <div>
+  //             ${message.forEach(msg => {
+  //                 return message
+  //             })}
+  //         </div>
+  //     </div>
+  //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  // </div>`
 
-}
+  if ($("#liveAlertPlaceholder").is(":empty")) {
+    alertPlaceholder.append(wrapper);
+  } else {
+    //If the event have been saved in less than 3 seconds, then the previous setTimeout will be deleted
+    //so the new alert has its own 3 seconds.
+    clearTimeout(timer);
+    $("#liveAlertPlaceholder").empty();
+    console.log("Emptied");
+    alertPlaceholder.append(wrapper);
+  }
+};
 
-const bgMusic = $('#bgMusic')[0];
-const musicBtn = $('#music-btn');
-const pauseImg = $('.pause-img');
-    bgMusic.volume = 0.05; // sets the volume to 5%
-$(musicBtn).on('click',function(){
-    if(bgMusic.paused){
-        bgMusic.play();
-        $(pauseImg).attr('src','/img/Simple_Music.-whitesvg.svg');
+const bgMusic = $("#bgMusic")[0];
+const musicBtn = $("#music-btn");
+const pauseImg = $(".pause-img");
+bgMusic.volume = 0.05; // sets the volume to 5%
+$(musicBtn).on("click", function () {
+  if (bgMusic.paused) {
+    bgMusic.play();
+    $(pauseImg).attr("src", "/img/Simple_Music.-whitesvg.svg");
+  } else {
+    bgMusic.pause();
+    $(pauseImg).attr("src", "/img/Simple_Music.-nosvg.svg");
+  }
+});
 
-    }else{
-        bgMusic.pause();
-        $(pauseImg).attr('src','/img/Simple_Music.-nosvg.svg');
-    }
-})
+$(document).ready(() => {
+  const date = new Date();
+  const today = date.toLocaleDateString();
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var day = date.getDate();
 
-$(document).ready(()=>{
-    const date = new Date();
-    const today = date.toLocaleDateString();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var  day = date.getDate();
-    
-    {month < 10 ? month = '0' + month : null};
-    {day < 10 ? day = '0' + day : null}
+  {
+    month < 10 ? (month = "0" + month) : null;
+  }
+  {
+    day < 10 ? (day = "0" + day) : null;
+  }
 
-    $('#birthdate').attr('max', `${year}-${month}-${day}`);
-
-
-})
+  $("#birthdate").attr("max", `${year}-${month}-${day}`);
+});
--- js/slides.js
+++ js/slides.js
@@ -1,35 +1,38 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-	new Splide( '#card-slider', {
-		perPage    : 4,
-		breakpoints: {
-			600: {
-				perPage: 1,
-			},
-			850:{
-				perPage: 2,
-			},
-			1280: {
-				perPage: 3,
-			},
-		},
-		'arrowPath': 'm15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z',
-		type: 'loop',
-	} ).mount();
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#card-slider", {
+    perPage: 4,
+    loop: false,
+    breakpoints: {
+      600: {
+        perPage: 1,
+      },
+      850: {
+        perPage: 2,
+      },
+      1280: {
+        perPage: 3,
+      },
+    },
+    arrowPath:
+      "m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z",
+    // type: 'loop',
+  }).mount();
 
-	new Splide( '#bebidas-slider', {
-		perPage    : 4,
-		breakpoints: {
-			600: {
-				perPage: 1,
-			},
-			850:{
-				perPage: 2,
-			},
-			1280: {
-				perPage: 3,
-			}
-		},
-		'arrowPath': 'm15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z',
-		type: 'loop',
-	} ).mount();
-} );
+  new Splide("#bebidas-slider", {
+    perPage: 4,
+    breakpoints: {
+      600: {
+        perPage: 1,
+      },
+      850: {
+        perPage: 2,
+      },
+      1280: {
+        perPage: 3,
+      },
+    },
+    arrowPath:
+      "m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z",
+    // type: "loop",
+  }).mount();
+});
